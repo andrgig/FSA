af42fff88ebc1994803ca8d61beead2c7a08e3a0
Andrea Gigli <andrgig@gmail.com>
2017-01-01 17:28:03 +0100

    Update 1_run_fast_feature_selection.sh

--- 1_run_fast_feature_selection.sh
+++ 1_run_fast_feature_selection.sh
@@ -8,12 +8,8 @@ python feature_selection/NGAS.py $ROOT_DIR
 # running improved greedy feature selection
 python feature_selection/XGAS.py $ROOT_DIR 
 
-# TODO: rendere script R parametrico nell'input
 # clustering data...
 Rscript feature_selection/hierarchical_clustering/hierarchical_clustering_wald.R $ROOT_DIR 
-
-# building feature file before running HCAS
-cat $ROOT_DIR/output/feature_rank.txt | grep -v "nan" | cut -f1 | cut -d't' -f2 | grep -v "label" > $ROOT_DIR/output/nonescludere.txt
 
 # running feature selection based on hierarchical clustering
 python feature_selection/HCAS.py $ROOT_DIR 
