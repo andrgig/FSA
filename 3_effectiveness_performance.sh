f365840017a9876758d644c0de5b246c47d23d25
Andrea Gigli <andrgig@gmail.com>
2017-01-01 18:21:16 +0100

    Update 3_effectiveness_performance.sh

--- 3_effectiveness_performance.sh
+++ 3_effectiveness_performance.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-ROOT_DIR=$1/
-NUM_FEATURES=$2
+ROOT_DIR=$1
+SELECTED_FEATURES_FILENAME=$2
 
-python test_feature_selection_performance/test_performance_lambdamart.py $ROOT_DIR/sample_train_features.txt \
-                                                                         $ROOT_DIR/sample_validation_features.txt \
-                                                                         $ROOT_DIR/sample_test_features.txt \
-                                                                         $ROOT_DIR
+python test_feature_selection_performance/test_performance_lambdamart.py $ROOT_DIR $SELECTED_FEATURES_FILENAME
 
 # TODO: inserire lettura lista trials...
 
